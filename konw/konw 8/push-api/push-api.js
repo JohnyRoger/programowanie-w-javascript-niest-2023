231b030847cf5ccbe2c2b8c7beefbf44a0a2d6f8
Rysiek Brzegowy <r.brzegowy@gmail.com>
2024-01-26 18:14:57 +0100

    konw 8 - push, notification

---
+++ konw/konw 8/push-api/push-api.js
@@ -0,0 +1,51 @@
+// ECDSA public key
+const appServerPublicKey = 'BGN1pj4Ypf7sTbYL9dj2YaeXbBuaPGO7nWZ1IdrBWnhQw5sx8oG5l0Y0RLsRkpV6patAlng6rs72M628ZFGHEOE'
+const pushServerSubUri = 'http://localhost:3000/api/subscribe-to-push'
+
+document.querySelector('#btn-ask-for-notification').addEventListener('click', appStart)
+
+async function appStart() {
+    // on user gesture!!!
+    await initializeNotifiactionApi()
+    const registration = await registerServiceWorker()
+    await subscribePushManager(registration)
+}
+async function registerServiceWorker() {
+    if (!('serviceWorker' in navigator)) {
+        throw new Error('Brak wsparcia dla ServiceWorker!')
+    }
+    if (!('PushManager' in window)) {
+        throw new Error('Brak wsparcia dla powiadomień Push!')
+    }
+    // Zarejestruj ServiceWorker (zwraca Promise)
+    return navigator.serviceWorker
+        .register('push-service-worker.js')
+        .then((registration) => {
+            console.log('[Push API SW] Registered', registration)
+            return registration
+        })
+        .catch((err) => console.log(err))
+}
+async function initializeNotifiactionApi() {
+    return Notification.requestPermission((status) => {
+        console.log('Status Notification API: ', status)
+    })
+}
+async function subscribePushManager(registration) {
+    const options = {
+        userVisibleOnly: true,
+        applicationServerKey: appServerPublicKey
+    }
+    registration.pushManager.subscribe(options)
+        .then(pushSubscription => {
+            console.log(pushSubscription)
+            return sendPushSubscriptionToServer(pushSubscription)
+        })
+}
+async function sendPushSubscriptionToServer(subscription) {
+    return fetch(pushServerSubUri, {
+        method: 'post',
+        headers: { 'Content-Type': 'Application/json' },
+        body: JSON.stringify(subscription)
+    })
+}
